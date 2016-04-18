dcc9da1b2ed5614f7f3da41d0a4f6fbe2ab38553
b-slavov <boyan.slavoff@gmail.com>
2016-04-18 11:17:46 +0300

    Autofac installed!

--- Source/Web/PremierLeagueApp.Web/Global.asax.cs
+++ Source/Web/PremierLeagueApp.Web/Global.asax.cs
@@ -8,6 +8,7 @@
     {
         protected void Application_Start()
         {
+            AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
