e816b4c752dca4c35d038740b5ae56c8b44a7efd
b-slavov <boyan.slavoff@gmail.com>
2016-04-19 11:17:34 +0300

    Integrated repository pattern and BaseModel{TKey} implemented!

---
+++ Source/Data/PremierLeagueApp.Data.Common/Models/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace PremierLeagueApp.Data.Common.Models
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
