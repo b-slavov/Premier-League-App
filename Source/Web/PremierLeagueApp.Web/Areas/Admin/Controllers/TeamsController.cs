4babd5779df84959b19bb9817dc08f1800193add
b-slavov <boyan.slavoff@gmail.com>
2016-05-10 23:40:11 +0300

    Fixed Admin Area!

---
+++ Source/Web/PremierLeagueApp.Web/Areas/Admin/Controllers/TeamsController.cs
@@ -0,0 +1,43 @@
+﻿namespace PremierLeagueApp.Web.Areas.Admin.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Data.Common;
+    using Data.Models;
+
+    public class TeamsController : Controller
+    {
+        private readonly IDbRepository<Club> teams;
+
+        public TeamsController(IDbRepository<Club> teams)
+        {
+            this.teams = teams;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View(this.teams.All());
+        }
+
+        public ActionResult TrackTeams()
+        {
+            var dbTeams = this.teams.All().ToList();
+            var trackedTeams = Crawler.TeamsInfo.GetPremierLeagueTeams();
+
+            foreach (var team in trackedTeams)
+            {
+                if (!dbTeams.Any(x => x.Name == team.Name))
+                {
+                    var newTeam = new Club { CreatedOn = DateTime.Now, Name = team.Name };
+                    this.teams.Add(newTeam);
+                }
+            }
+
+            this.teams.Save();
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
